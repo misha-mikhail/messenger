34c2f782833b95f8b6303474a3b2a3575b281e14
Mikhail Kostylev <mixakos98@gmail.com>
2020-02-29 22:27:54 +0300

    добавил ендпоинты-заглушки

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,9 @@
 import server from './server';
 
 require('./plugins/users');
+require('./plugins/messages');
+require('./plugins/contacts');
+require('./plugins/auth');
 
 server.listen(3000, function (err, address) {
     if (err) {
--- packages/server/src/plugins/users.ts
+++ packages/server/src/plugins/users.ts
@@ -1,10 +1,21 @@
 import server from '../server';
 import { User } from '@chat/shared';
 
+
 server.get('/users/get', function (request, reply) {
     const user: User = {
         Username: 'someUserName',
     };
-
+    
     reply.send(user);
 });
+
+server.patch('/users/edit-bio', function (request, reply) {
+    const user: User = {
+        Bio: 'This is the edited bio !',
+    };
+    
+    reply.send(user);
+});
+
+    
