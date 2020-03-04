7056dc8bf6f22d17c5cb4bdfd0484b8f457a1c94
Михаил Гунин <Mikhail.Gunin@rl-inform.com>
2020-03-04 21:03:44 +0300

    Fix #5: Создать первую полноценную коллекцию в БД:
    + Добавлен "emitDecoratorMetadata": true в tsconfig'и, чтобы типы колонок резолвились из тайпскрипта.
    + Запилен маршрут, который создаёт тестовую запись, и маршрут, который получает все записи коллекции User.
    + Добавлен User в connection.entities [ ... ].
    + Пофикшены некоторые npm-scripts.

--- packages/server/src/database/connection.ts
+++ packages/server/src/database/connection.ts
@@ -1,10 +1,14 @@
 import {createConnection} from 'typeorm';
+import { User } from '@chat/shared';
 
 export default async function() {
     return await createConnection({
-        type: "mongodb",
-        host: "localhost",
+        type: 'mongodb',
+        host: 'localhost',
         port: 27017,
-        database: "chat"
+        database: 'chat',
+        entities: [
+            User,
+        ]
     });
 }
--- packages/server/src/plugins/users.ts
+++ packages/server/src/plugins/users.ts
@@ -1,10 +1,24 @@
 import server from '../server';
 import { User } from '@chat/shared';
+import { getConnection } from 'typeorm';
 
-server.get('/users/get', function (request, reply) {
-    const user = {
-        Username: 'someUserName',
-    };
+server.get('/users/get', async function (request, reply) {
+    const repo = getConnection().getRepository(User);
 
-    reply.send(user);
+    const allUsers = await repo.find();
+
+    reply.send(allUsers);
+});
+
+server.get('/users/make-test-single', async function (request, reply) {
+    const repo = getConnection().getRepository(User);
+
+    const user = new User();
+    Object.assign(user, {
+        Bio: 'safasf',
+        Username: 'asfas',
+    })
+    await repo.save(user);
+
+    reply.send({user});
 });
