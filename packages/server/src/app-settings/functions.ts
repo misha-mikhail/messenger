b56d9ca536433cab9f43b5ef00e0895f4cc89a15
Mikhail Gunin <gunka462@gmail.com>
2020-04-10 11:33:22 +0300

    + Извлёк FileExtensions.readJsonFile<TJson>(filePath: string)

--- packages/server/src/app-settings/functions.ts
+++ packages/server/src/app-settings/functions.ts
@@ -1,10 +1,6 @@
-import { promises } from 'fs';
 import { AppSettingsJson } from './AppSettingsJson';
+import { FileExtensions } from '../extensions/FileExtensions';
 
 export async function readAppSettings() : Promise<AppSettingsJson | null> {
-    const appSettingsFile = await promises.readFile('appSettings.json');
-
-    if (!appSettingsFile.length) return null;
-
-    return JSON.parse(appSettingsFile.toString()) as AppSettingsJson;
+    return await FileExtensions.readJsonFile<AppSettingsJson>('appSettings.json');
 }
