5f399a07aaca0446157e13f7685483340543aac6
Mikhail Gunin <gunka462@gmail.com>
2020-04-22 01:58:17 +0300

    Отлично! Job фейлится, если проект не билдится. Всё как надо.
    This reverts commit 562c505be9f0a34d7e3d227dd339486ab04ba7c4.

--- packages/server/src/auth/jwt.ts
+++ packages/server/src/auth/jwt.ts
@@ -1,6 +1,6 @@
 import { readAppSettings } from '../app-settings';
 
-это строчка, которая точно не даст сбилдиться проекту!
+
 export async function getJwtSecret() {
     const appSettings = await readAppSettings();
     return appSettings.Jwt.Secret;
