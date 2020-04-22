5ba2525deb191c6b44d2cce4b66165c30a005975
Мишка <mixakos98@yandex.ru>
2020-04-23 00:19:22 +0300

    Resolve #22: Прикручен логгер, логи пока только в терм (#55)
    Fix #22: Прикручен логгер
    
    +логгирует в терминал и в файл
    
    Co-authored-by: Костылев Михаил <mixakos98@gmail.com>

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,12 +3,14 @@ import { getJwtSecret } from './auth';
 import { UserRepository } from './database/repositories';
 import {Container} from "typedi";
 import { ContainerKeys } from './constants';
+import 'reflect-metadata';
 
 export async function startApplication(port: number) { 
     useContainer(Container);
     Container.set(ContainerKeys.jwtSecret, await getJwtSecret());
 
     const app = createKoaServer({
+        middlewares: [ __dirname + '/middlewares/*.js'],
         cors: true,
         controllers: [ __dirname + '/controllers/*.js' ],
         authorizationChecker: async (action: Action, _roles: string[]) => {
@@ -22,6 +24,7 @@ export async function startApplication(port: number) {
             return userRepo.findUserByToken(token);
         },
     });
+
 
     app.listen(port);
 
