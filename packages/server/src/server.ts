ea60806f1acb7ae0c7ff326eac3ec82acd9eb0fc
Mikhail Gunin <gunka462@gmail.com>
2020-05-01 02:23:07 +0300

    Resolve #25: Прикручен дебаг @chat/server через вскод. Смотреть сюда: https://github.com/Microsoft/vscode-recipes/tree/master/nodemon
    + Пофикшены остальные скрипты запуска и билда.
    С ts-node не нужно билдить TS постоянно.

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,16 +1,19 @@
 import { createKoaServer, Action, useContainer } from 'routing-controllers';
 import { getJwtSecret } from './auth';
 import { UserRepository } from './database/repositories';
-import {Container} from "typedi";
+import { Container } from "typedi";
 import { ContainerKeys } from './constants';
 
-export async function startApplication(port: number) { 
+export async function startApplication(port: number) {
     useContainer(Container);
     Container.set(ContainerKeys.jwtSecret, await getJwtSecret());
 
     const app = createKoaServer({
         cors: true,
-        controllers: [ __dirname + '/controllers/*.js' ],
+        controllers: [
+            __dirname + '/controllers/*.js',
+            __dirname + '/controllers/*.ts', // for ts-node
+        ],
         authorizationChecker: async (action: Action, _roles: string[]) => {
             const userRepo = Container.get(UserRepository);
             const token = action.request.headers['authorization'];
